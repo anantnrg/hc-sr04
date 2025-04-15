671c71530f3a2969819b0e58172e7e65c52c37df
Anant Narayan <anantnrg@gmail.com>
2025-04-15 11:55:08 +0530

    impl dist function

--- src/lib.rs
+++ src/lib.rs
@@ -20,21 +20,37 @@ where
         Self { trig, echo, delay }
     }
 
-    pub fn dist(&mut self) -> Result<u32, ()> {
-        self.trig.set_low().map_err(|_| ())?;
+    pub fn dist(&mut self) -> Result<u32, Error> {
+        self.trig.set_low().map_err(|_| Error::Gpio)?;
         self.delay.delay_us(2);
-        self.trig.set_high().map_err(|_| ())?;
+        self.trig.set_high().map_err(|_| Error::Gpio)?;
         self.delay.delay_us(10);
-        self.trig.set_low().map_err(|_| ())?;
+        self.trig.set_low().map_err(|_| Error::Gpio)?;
 
         let mut timeout = 30_000;
 
-        while self.echo.is_low().map_err(|_| ())? {
+        while self.echo.is_low().map_err(|_| Error::Gpio)? {
             timeout -= 1;
             if timeout == 0 {
-                return Err(());
+                return Err(Error::Timeout);
             }
         }
-        Ok(0)
+
+        let mut count = 0;
+
+        while self.echo.is_high().map_err(|_| Error::Gpio)? {
+            count += 1;
+            if count > 30_000 {
+                return Err(Error::Timeout);
+            }
+        }
+        Ok(count / 58)
     }
+}
+
+/// Errors that can happen during a measurement
+#[derive(Debug)]
+pub enum Error {
+    Timeout,
+    Gpio,
 }
